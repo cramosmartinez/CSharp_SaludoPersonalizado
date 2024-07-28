a5e6eec50e5f7209e2483d6b817a2057286ef5c7
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-28 10:29:27 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_SaludoPersonalizado/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, introduce tu nombre: ");
+        string nombre = Console.ReadLine();
+
+        if (nombre.Equals("Alejandro", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("Hola");
+        }
+        else
+        {
+            Console.WriteLine("No te conozco");
+        }
+    }
+}
